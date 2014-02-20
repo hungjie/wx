371a14995b2b8163de7e1539aa0f576ac1557102
liuhongzhao <liuhongzhao@zingmobile.com>
2014-02-20 17:51:57 +0800

    get order items change

--- service/msisdn_infos.php
+++ service/msisdn_infos.php
@@ -11,7 +11,9 @@ class msisdnProcess{
     function get_cur_order($msisdn){
         $db = get_db();
         $db->begin_query();
+        $today_date = date("Y-m-d");
         $res = $db->table('user_order')->where(array('user_id'=>$msisdn, 'status'=>0))
+                ->where("time_at > '$today_date'")
                 ->order_by_desc('id')
                 ->exec();
         
